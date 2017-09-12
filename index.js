61b7a5cb36a7ebeacd9480a78107973c6d4183e5
Anthony deguzman110@gmail.com <deguzman110@gmail.com>
2017-09-12 16:06:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyholidaysTo(name) {
-  return "Happy holidays, ${name}!";
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
